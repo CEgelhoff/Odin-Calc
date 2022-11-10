0d162058b0b83f01281de2eab58f98e0c57fdd49
CEgelhoff <christoph.egelhoff@gmail.com>
2022-11-10 12:12:01 -0700

    basic layout is assembled

---
+++ calc.js
@@ -0,0 +1,19 @@
+function add(a,b) {
+    return a + b;    
+}
+
+function sub(c,d) {
+    return c - d;
+}
+
+function mult(e,f) {
+    return e * f;
+}
+
+function divide(g,h) {
+    return g / h;
+}
+
+function operate(operator, x, y) {
+     
+}
